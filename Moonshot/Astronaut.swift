260510d1f907fee583c430940efb8f1ab1f8145b
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-09-13 21:16:37 +0200

    App in progress

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Maciej Wiącek on 13/09/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
