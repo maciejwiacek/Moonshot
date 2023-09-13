cf283cb2c75303d3b29a2bdd1102ba04e2cc0ad9
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-09-13 17:56:51 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Maciej Wiącek on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
